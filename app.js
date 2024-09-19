018f1ae0155c23619fecf53773b92e16310a918e
izu-home <mizzuddin@strateqgroup.com>
2024-09-20 04:30:49 +0800

    change is imminent

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello THIS HAS BEEN CHANGED AND NEED TO BE REVIEW ; 2nd test'));
+app.get('/', (req, res) => res.send('Hello this is izzu 2nd attempt on codebuild'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
